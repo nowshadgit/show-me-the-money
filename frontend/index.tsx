1e9a41c9340d6ab152e238ab79e8fbb6f942c0e9
Nowshad <nowsad19979@gmail.com>
2025-05-11 23:38:14 +0530

    Resolved two times API call execution

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom/client";
 import BalanceSheet from "./src/BalanceSheet";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BalanceSheet />
-  </React.StrictMode>
+  <BalanceSheet />
 );
